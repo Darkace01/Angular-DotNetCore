97f25a9cefda83834411afebd34ed17c0af239fb
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-04-20 16:40:14 +0100

    ngIf and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    setTimeout(() => {
+      this.movies = [
+        {
+          title: 'James Bond',
+          releaseDate: new Date(),
+          price: 145.99
+        },
+        {
+          title: 'Vercel',
+          releaseDate: new Date(),
+          price: 445.99
+        },
+        {
+          title: 'Bobo yii',
+          releaseDate: new Date('2021-10-12'),
+          price: 135.99
+        }
+      ];
+    }, 1000);
+  }
+
+
+
   title = 'angular-movies';
+  movies: any;
+  duplicateNumber(n: number) {
+    return n * 2
+  }
 }
