4d79ba98409b89812f101e98481f21ee81388fbb
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-04-21 11:41:46 +0100

    Code Clean Up
    Removed un used components and styled the movies component also adding poster link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,29 @@ export class AppComponent implements OnInit {
       {
         title: 'James Bond',
         releaseDate: new Date(),
-        price: 145.99
+        price: 145.99,
+        poster: 'https://m.media-amazon.com/images/M/MV5BMTk3ZGM2YzQtMmYxYy00NDZjLWFmOTUtZTQwMzg0ZjA5ZjU3XkEyXkFqcGdeQXVyODk2NDQ3MTA@._V1_UX182_CR0,0,182,268_AL_.jpg'
       },
       {
-        title: 'Bobo yii',
+        title: 'Venom',
         releaseDate: new Date('2021-10-12'),
-        price: 135.99
+        price: 135.99,
+        poster: 'https://m.media-amazon.com/images/M/MV5BNzAwNzUzNjY4MV5BMl5BanBnXkFtZTgwMTQ5MzM0NjM@._V1_UX182_CR0,0,182,268_AL_.jpg'
       }
     ];
 
     this.moviesTrending = [
       {
-        title: 'Awon Boys',
+        title: 'Law and Order',
         releaseDate: new Date(),
-        price: 145.99
+        price: 145.99,
+        poster: 'https://m.media-amazon.com/images/M/MV5BZmI3OWY5OWYtNjkyOC00MTZkLTliYTMtMmU0NTU5MGQ1MDMwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
       },
       {
-        title: 'Talo yii',
+        title: 'Bad Boys For Life',
         releaseDate: new Date('2021-10-12'),
-        price: 135.99
+        price: 135.99,
+        poster: 'https://m.media-amazon.com/images/M/MV5BMWU0MGYwZWQtMzcwYS00NWVhLTlkZTAtYWVjOTYwZTBhZTBiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
       }
     ];
   }
