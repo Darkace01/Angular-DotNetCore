a12baccc0d194ef94d682b50488eefd7413be204
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-04-22 09:18:04 +0100

    Reacting to changes

--- src/app/movies/movie-filter/movie-filter.component.ts
+++ src/app/movies/movie-filter/movie-filter.component.ts
@@ -14,16 +14,51 @@ export class MovieFilterComponent implements OnInit {
 
   genres = [{ id: 1, name: 'Drama' }, { id: 2, name: 'Action' }];
 
+  movies = [
+    {
+      title: 'James Bond',
+      releaseDate: new Date(),
+      price: 145.99,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTk3ZGM2YzQtMmYxYy00NDZjLWFmOTUtZTQwMzg0ZjA5ZjU3XkEyXkFqcGdeQXVyODk2NDQ3MTA@._V1_UX182_CR0,0,182,268_AL_.jpg'
+    },
+    {
+      title: 'Venom',
+      releaseDate: new Date('2021-10-12'),
+      price: 135.99,
+      poster: 'https://m.media-amazon.com/images/M/MV5BNzAwNzUzNjY4MV5BMl5BanBnXkFtZTgwMTQ5MzM0NjM@._V1_UX182_CR0,0,182,268_AL_.jpg'
+    },
+    {
+      title: 'Inception',
+      releaseDate: new Date('2021-10-12'),
+      price: 135.99,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg'
+    }
+  ]
+
+  originalMovies = this.movies;
+
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       title: '',
       genreId: 0,
       upcomingReleases: false,
       inTheaters: false
-    })
+    });
+
+    this.form.valueChanges
+      .subscribe(values => {
+        this.movies = this.originalMovies;
+        this.filterMovies(values);
+      })
+  }
+
+  filterMovies(values: any) {
+    if (values.title) {
+      this.movies = this.movies.filter(movie => movie.title.toLowerCase().indexOf(values.title.toLowerCase()) !== -1);
+    }
   }
 
   clearForm() {
-
+    this.form.reset();
   }
 }
