3ed43b72e4fad409a5f84a256639a44abb9c64ab
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-05-06 09:00:50 +0100

    Movies edit and create form

--- src/app/movies/edit-movies/edit-movies.component.ts
+++ src/app/movies/edit-movies/edit-movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { movieCreationDTO, movieDTO } from '../movies.model';
 
 @Component({
   selector: 'app-edit-movies',
@@ -10,9 +11,17 @@ export class EditMoviesComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
+  model: movieDTO = {
+    title: 'Eternals', inTheaters: true, summary: 'Dude really',
+    releaseDate: new Date(), trailer: 'ABBUU', poster: 'https://m.media-amazon.com/images/M/MV5BOTYzMTlhM2ItMmFkYi00ZTJhLTg3MWQtNzM1NTRkM2NiNzRjXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_UY268_CR8,0,182,268_AL_.jpg'
+  }
+
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
     });
   }
 
+  saveChanges(movieCreationDTO: movieCreationDTO) {
+
+  }
 }
