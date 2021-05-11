e21b619d572aea736dce0a2e83fcd1cfc5ef8462
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-05-11 13:52:27 +0100

    Display Error from the API

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { parseWebAPIErrors } from 'src/app/utilities/utils';
 import { genreCreateDTO } from '../genres.module';
 import { GenresService } from '../genres.service';
 
@@ -10,6 +11,8 @@ import { GenresService } from '../genres.service';
 })
 export class CreateGenreComponent implements OnInit {
 
+  errors: string[] = [];
+
   constructor(private router: Router, private genreService: GenresService) { }
 
 
@@ -19,7 +22,7 @@ export class CreateGenreComponent implements OnInit {
   saveChanges(genreCreateDTO: genreCreateDTO) {
     this.genreService.create(genreCreateDTO).subscribe(() => {
       this.router.navigate(['/genres']);
-    }, error => console.error(error));
+    }, error => this.errors = parseWebAPIErrors(error));
 
   }
 
