7c350c604c7e27776c8b5fa90382b52e8be3d16b
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-05-19 11:05:17 +0100

    Indexing movietheaters

--- src/app/movie-theater/index-movie-theater/index-movie-theater.component.ts
+++ src/app/movie-theater/index-movie-theater/index-movie-theater.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { movieTheatersDTO } from '../movie-theater.model';
+import { MovieTheaterService } from '../movie-theater.service';
 
 @Component({
   selector: 'app-index-movie-theater',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexMovieTheaterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private movieTheaterService: MovieTheaterService) { }
+
+  movieTheaters!: movieTheatersDTO[];
+
+  columnsToDisplay = ['name', 'actions'];
 
   ngOnInit(): void {
+    this.movieTheaterService.get().subscribe(movieTheaters => this.movieTheaters = movieTheaters);
   }
 
+  delete(id: number) {
+
+  }
 }
--- src/app/movie-theater/movie-theater.service.ts
+++ src/app/movie-theater/movie-theater.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { movieTheatersCreationDTO } from './movie-theater.model';
+import { movieTheatersCreationDTO, movieTheatersDTO } from './movie-theater.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class MovieTheaterService {
   constructor(private http: HttpClient) {
   }
   private apiURL = environment.apiURL + '/movieTheater';
+
+  public get(): Observable<movieTheatersDTO[]> {
+    return this.http.get<movieTheatersCreationDTO[]>(this.apiURL);
+  }
 
   public create(movieTheaterDTO: movieTheatersCreationDTO) {
     return this.http.post(this.apiURL, movieTheaterDTO);
